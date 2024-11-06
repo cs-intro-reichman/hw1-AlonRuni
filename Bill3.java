c8f958b369b948916e0f7c065202f64016c896e1
Alon Hagity <alonhagity@Alons-MacBook-Air.local>
2024-11-06 17:12:47 +0200

    minor changes

--- Bill3.java
+++ Bill3.java
@@ -1,13 +1,13 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-	    String name1 = args[0];
-		String name2 = args[1];
-		String name3 = args[2];
+	    String a = args[0];
+		String b = args[1];
+		String c = args[2];
 		double amount = Double.parseDouble(args[3]);
 		double amountPerPerson = Math.ceil(amount/3);
 
-	    String str = "Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + amountPerPerson + " Shekels each.";
+	    String str = "Dear " + c + ", " + b + " and " + a + ": pay " + amountPerPerson + " Shekels each.";
 		System.out.println(str);
 	}
 }
